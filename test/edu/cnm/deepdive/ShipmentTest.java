7e0cf3a04d7134c0881a83f105662c2f06229ccf
Felipe Cook <felipepcook@gmail.com>
2019-04-09 20:30:05 -0600

    began attempt on unit tests

---
+++ test/edu/cnm/deepdive/ShipmentTest.java
@@ -0,0 +1,24 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShipmentTest {
+
+  @Test
+  void getInitiation() {
+  }
+
+  @Test
+  void setInitiation() {
+  }
+
+  @Test
+  void getTracking() {
+  }
+
+  @Test
+  void addTracking() {
+  }
+}
---
+++ test/edu/cnm/deepdive/TrackingInfoTest.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TrackingInfoTest {
+
+  @Test
+  void getRecorded() {
+  }
+
+  @Test
+  void getDetails() {
+  }
+}
